a2a513d53a849e31341555f82666795dcbf8c5f1
HP <HP@Notlitefrost>
2017-12-17 17:59:26 +0700

    TambahBukuContext,EditBukuContext,HapusBukuContext(commiter : whitzkers)

--- src/main/java/com/mycompany/repository/AnggotaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.repository;
-
-import com.mycompany.model.Anggota;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AnggotaRepository extends CrudRepository<Anggota, Integer>{
-    Iterable<Anggota> findById(Integer id);
-}
